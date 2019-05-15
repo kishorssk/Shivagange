72c2d1f7f511fed9b780ff16a1e0b31a09d0deac
kishorssk <kishor.sathigrama@gmail.com>
2019-05-15 18:58:37 +0530

    gradle updated and one more alubm added

--- app/src/main/java/com/srirangadigital/shivagange/About.java
+++ app/src/main/java/com/srirangadigital/shivagange/About.java
@@ -1,10 +1,15 @@
 package com.srirangadigital.shivagange;
 
 import android.media.MediaPlayer;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class About extends AppCompatActivity {
 
@@ -14,13 +19,49 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initCollapsingToolbar();
+
         hhSpeech = MediaPlayer.create(this, R.raw.hhsriswamiji);
 
+        try {
+            Glide.with(this).load(R.drawable.mutt).into((ImageView) findViewById(R.id.backdrop1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar1);
+        appBarLayout.setExpanded(true);
+
+        // hiding & showing the title when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbar.setTitle(getString(R.string.title_activity_about));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
 
     public void playIT(View v){
 
@@ -32,4 +73,5 @@ public class About extends AppCompatActivity {
         super.onPause();
         hhSpeech.release();
     }
+
 }
